6113a68aef7bb20ed8d02f7b0d6864b07634c17f
regular1020 <pjg1020@kookmin.ac.kr>
2021-05-05 22:36:15 +0900

    2021.05.05
    문제 : 124나라

---
+++ 124/solution.py
@@ -0,0 +1,13 @@
+def solution(n):
+    list = ["4", "1", "2"]
+    answer = ''
+    while n>0:
+        answer = list[n % 3] + answer
+        if n%3 == 0:
+            n = n-1
+        n = n//3
+    return answer
+
+if __name__ == '__main__':
+    num = int(input())
+    print(solution(num))
---
+++ CompareAndDelete/solution.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = 0
+    stack = []
+    for i in range(len(s)):
+        if len(stack) != 0:
+            tmp = stack.pop()
+            if tmp != s[i]:
+                stack.append(tmp)
+                stack.append(s[i])
+        else:
+            stack.append(s[i])
+    if len(stack) == 0:
+        answer = 1
+    return answer
+
+if __name__ == '__main__':
+    sent = input()
+    ans = solution(sent)
+    print(ans)
